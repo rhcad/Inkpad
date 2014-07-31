6856c85e503fa57a7fb58e96896d57d79ad013f6
Zhang Yungui <rhcad@hotmail.com>
2014-07-31 16:08:04 +0800

    注释 WDPickResult.h，增加 WDPickFlag、WDNodeType 类型，以前为匿名枚举定义

--- Inkpad-Core/Model/WDPickResult.h
+++ Inkpad-Core/Model/WDPickResult.h
@@ -11,53 +11,57 @@
 
 #import <Foundation/Foundation.h>
 
-enum {
-    kWDSnapNodes            = 1 << 0,
-    kWDSnapEdges            = 1 << 1,
-    kWDSnapGrid             = 1 << 2,
-    kWDSnapFills            = 1 << 3,
-    kWDSnapLocked           = 1 << 4,
-    kWDSnapSelectedOnly     = 1 << 5,
-    kWDSnapSubelement       = 1 << 6,
-    kWDSnapDynamicGuides    = 1 << 7
-};
-
+// 捕捉开关，用于选择图形或坐标捕捉
 typedef enum {
-    kWDEther,
-    kWDInPoint,
-    kWDAnchorPoint,
-    kWDOutPoint,
-    kWDObjectFill,
-    kWDEdge,
-    kWDLeftTextKnob,
-    kWDRightTextKnob,
-    kWDFillStartPoint,
-    kWDFillEndPoint,
-    kWDRectCorner,
-    kWDRectEdge,
-    kWDTextPathStartKnob
+    kWDSnapNodes            = 1 << 0,   // 允许吸附到点
+    kWDSnapEdges            = 1 << 1,   // 允许吸附到边
+    kWDSnapGrid             = 1 << 2,   // 允许吸附到网格
+    kWDSnapFills            = 1 << 3,   // 允许在路径内部捕捉
+    kWDSnapLocked           = 1 << 4,   // 允许捕捉锁定的图形
+    kWDSnapSelectedOnly     = 1 << 5,   // 仅在当前选中的图形(含其上级图形)内捕捉
+    kWDSnapSubelement       = 1 << 6,   // 允许穿透捕捉
+    kWDSnapDynamicGuides    = 1 << 7    // 使用智能参考线定位坐标
+} WDPickFlag;
+
+// 点选结果类型
+typedef enum {
+    kWDEther,                           // 没有选中
+    kWDInPoint,                         // 左控制手柄，位于锚点进入方向
+    kWDAnchorPoint,                     // 锚点
+    kWDOutPoint,                        // 右控制手柄，位于锚点出去方向
+    kWDObjectFill,                      // 捕捉到路径内部
+    kWDEdge,                            // 捕捉到边
+    kWDLeftTextKnob,                    // 文本框左控制手柄
+    kWDRightTextKnob,                   // 文本框右控制手柄
+    kWDFillStartPoint,                  // 渐变开始控制手柄
+    kWDFillEndPoint,                    // 渐变结束控制手柄
+    kWDRectCorner,                      // 矩形角点
+    kWDRectEdge,                        // 矩形的边
+    kWDTextPathStartKnob                // 沿线文本的起始控制手柄，位于起始线顶端
 } WDPickResultType;
 
-enum {
-    kWDMiddleNode,
-    kWDFirstNode,
-    kWDLastNode
-};
+// 点选结果的锚点位置类型
+typedef enum {
+    kWDMiddleNode,                      // 中间锚点
+    kWDFirstNode,                       // 首锚点
+    kWDLastNode                         // 末锚点
+} WDPickNodePosition;
 
-extern const float kNodeSelectionTolerance;
+extern const float kNodeSelectionTolerance; // 选择容差，捕捉框宽度
 
 @class WDElement;
 @class WDBezierNode;
 
+// 点选结果，用于选择图形或坐标捕捉
 @interface WDPickResult : NSObject
 
 @property (nonatomic, weak) WDElement *element;         // the element in which the tap occurred
 @property (nonatomic, weak) WDBezierNode *node;         // the node hit by the tap -- could be nil
-@property (nonatomic, assign) CGPoint snappedPoint;
-@property (nonatomic, assign) WDPickResultType type;
-@property (nonatomic, assign) NSUInteger nodePosition;
-@property (nonatomic, readonly) BOOL snapped;
+@property (nonatomic, assign) CGPoint snappedPoint;     // 捕捉到的坐标, snapped 为真时有效
+@property (nonatomic, assign) WDPickResultType type;    // 点选结果类型
+@property (nonatomic, assign) NSUInteger nodePosition;  // WDPickNodePosition, type 为 kWDAnchorPoint 时有效
+@property (nonatomic, readonly) BOOL snapped;           // 是否捕捉到坐标
 
-+ (WDPickResult *) pickResult;
++ (WDPickResult *) pickResult;                          // 构造一个空的结果
 
 @end
